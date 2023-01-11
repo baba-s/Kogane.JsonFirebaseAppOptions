b2f0ffe3cfc382bdb855bda82731689c7f83a9aa
baba-s <shotababa1206@gmail.com>
2023-01-11 20:28:48 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Kogane.JsonFirebaseAppOptions
+# Kogane Json Firebase App Options
+
+Firebase の AppOptions クラスを JSON 形式の文字列に変換できるパッケージ
+
+## 使用例
+
+```cs
+using Firebase;
+using Kogane;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private void Start()
+    {
+        var firebaseApp = FirebaseApp.DefaultInstance;
+        var options     = firebaseApp.Options;
+
+        Debug.Log( new JsonFirebaseAppOptions( options ) );
+    }
+}
+```
---
+++ Runtime/JsonFirebaseAppOptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Firebase;
+using UnityEngine;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable NotAccessedField.Local
+
+namespace Kogane
+{
+    [Serializable]
+    public struct JsonFirebaseAppOptions
+    {
+        [SerializeField] private string databaseUrl;
+        [SerializeField] private string appId;
+        [SerializeField] private string apiKey;
+        [SerializeField] private string messageSenderId;
+        [SerializeField] private string storageBucket;
+        [SerializeField] private string projectId;
+
+        public JsonFirebaseAppOptions( AppOptions options )
+        {
+            databaseUrl     = options.DatabaseUrl?.ToString();
+            appId           = options.AppId;
+            apiKey          = options.ApiKey;
+            messageSenderId = options.MessageSenderId;
+            storageBucket   = options.StorageBucket;
+            projectId       = options.ProjectId;
+        }
+
+        public override string ToString()
+        {
+            return JsonUtility.ToJson( this, true );
+        }
+    }
+}
